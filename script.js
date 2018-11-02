261bfe3c999feb61f6902e74c93c2899885e550d
olha_ko <mismire9@gmail.com>
2018-11-02 18:40:31 +0200

    adding the solution to the exercise

---
+++ script.js
@@ -0,0 +1,45 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    var newButton = document.createElement("button");
+    newButton.appendChild(document.createTextNode("Delete"));
+    newButton.classList.add("del");
+    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(newButton);
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+        createDeleteButton();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+        createDeleteButton();
+    }
+}
+
+ul.addEventListener("click", e => {
+    if (e.target.localName === "button") {
+        e.target.parentNode.remove();
+    } else if (e.target.localName === "li") {
+        e.target.classList.toggle("done");
+    };
+});
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
